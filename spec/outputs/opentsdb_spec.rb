14e8a58bd5106a43f1c3091e180a71222f2a208d
Felix Aronsson <felixaronsson@gmail.com>
2016-05-29 00:09:08 +0200

    Add test to make sure we reconnect on puts error

--- spec/outputs/opentsdb_spec.rb
+++ spec/outputs/opentsdb_spec.rb
@@ -39,4 +39,24 @@ describe LogStash::Outputs::Opentsdb do
       expect(subject.socket.pop).to match(/^put metric_1 \d+ 123 tag1=foo tag2=bar\n/)
     end
   end
+
+  context 'socket.puts fails with ECONNRESET' do
+    let(:config) do
+      {
+        'metrics' => ['metric_1', '%{val}']
+      }
+    end
+    let(:event) { LogStash::Event.new('val' => 123) }
+
+    before do
+      subject.register
+    end
+
+    it 'tries to reconnect' do
+      allow(subject.socket).to receive(:puts).and_raise(Errno::ECONNRESET)
+      expect(subject).to receive(:connect)
+
+      subject.receive event
+    end
+  end
 end
